207d77dc2332b7d68d8deaffe4dc924c4c7d31e8
MstrMinecrafter <coxsimon621@gmail.com>
2026-06-01 15:14:20 -0700

    Smoothed screen transitions

--- root.gd
+++ root.gd
@@ -4,6 +4,10 @@ extends Node2D
 var screenId = 1
 @onready var camera = $Camera2D
 
+var prevScreenId = 1
+var transitionTotalTime = 0.75
+var transitionTime = -1
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,10 +15,22 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("right"):
-		screenId += 1
-	if Input.is_action_just_pressed("left"):
-		screenId -= 1
-	screenId = clamp(screenId, 0, 2)
-	camera.global_position = screens[screenId].global_position
-	pass
+	if transitionTime > -1 && transitionTime < transitionTotalTime:
+		transitionTime += delta
+		camera.global_position = smooth_lerp(screens[prevScreenId].global_position, screens[screenId].global_position, transitionTime/transitionTotalTime)
+	else:
+		if Input.is_action_just_pressed("right"):
+			transition_screens(1)
+		if Input.is_action_just_pressed("left"):
+			transition_screens(-1)
+
+func smooth_lerp(from: Vector2, to: Vector2, x: float) -> Vector2:
+	return from + (to - from) * smooth_func(x)
+
+func smooth_func(x: float) -> float:
+	return sin(PI * (x - 0.5))/2 + 0.5
+
+func transition_screens(dir: int) -> void:
+	prevScreenId = screenId
+	screenId = clamp(screenId + dir, 0, 2)
+	transitionTime = 0
