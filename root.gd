1f767d77728ad08e873c293d94c4626648895374
MstrMinecrafter <coxsimon621@gmail.com>
2026-06-01 14:42:45 -0700

    Changed screen-switch control scheme

--- root.gd
+++ root.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var screens: Array[Node2D] = [$Deck,$Table,$Store]
 var screenId = 1
 @onready var camera = $Camera2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -10,9 +11,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_right"):
+	if Input.is_action_just_pressed("right"):
 		screenId += 1
-	if Input.is_action_just_pressed("ui_left"):
+	if Input.is_action_just_pressed("left"):
 		screenId -= 1
 	screenId = clamp(screenId, 0, 2)
 	camera.global_position = screens[screenId].global_position
