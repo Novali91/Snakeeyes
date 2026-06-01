87f9606423a4289d8add11bce8495eee346a5cf5
Novali91 <joeyd@firetruk.ca>
2026-06-01 16:49:55 -0700

    Made drink/snake classes and filesystem stuff

--- 02_Deck/01_Drinks/drink.gd
+++ 02_Deck/01_Drinks/drink.gd
@@ -1,4 +1,5 @@
 extends Node
+class_name Drink
 
 @export var poison: int
 @export var charm: int
--- 02_Deck/02_Snakes/snake.gd
+++ 02_Deck/02_Snakes/snake.gd
@@ -1,4 +1,5 @@
 extends Node
+class_name Snake
 
 @export var attached_drink: Node
 @export var cost: int
