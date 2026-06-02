805cf5db27a26ddaa6d983b4b80cc263e162fd72
Novali91 <joeyd@firetruk.ca>
2026-06-02 08:50:59 -0700

    State manager/states outlines created

---
+++ 01_Managers/01_StateManager/01_States/01_Start/start.gd
@@ -0,0 +1,12 @@
+extends Node
+
+## Starting state: I imagine this is where we do upkeep/etc?
+## One piece of potential upkeep: drink_manager new_turn() function
+
+## I'm guessing this will be called by state_manager when it switches to this state
+func ready() -> void:
+	pass
+
+## I'm guessing this will be called in state_manager physics_process
+func tick(delta: float) -> void:
+	pass
---
+++ 01_Managers/01_StateManager/01_States/09_YouWin/you_win.gd
@@ -0,0 +1,11 @@
+extends Node
+
+## You win the arm wrestle state :)
+
+## I'm guessing this will be called by state_manager when it switches to this state
+func ready() -> void:
+	pass
+
+## I'm guessing this will be called in state_manager physics_process
+func tick(delta: float) -> void:
+	pass
