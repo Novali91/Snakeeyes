6bedc3a084d9c5219a9bb55538c934f5d3eb11f7
MstrMinecrafter <coxsimon621@gmail.com>
2026-06-02 16:04:35 -0700

    Buncha bullshit

--- root.gd
+++ root.gd
@@ -12,7 +12,7 @@ var transitionTime = -1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	SCREEN_SIZE = camera.get_viewport_rect().size
-	$Table.slide_cups([Drink.new(),Drink.new(),Drink.new(),Drink.new()] as Array[Drink])
+	$Table.begin_slide_cups([Drink.new(),Drink.new(),Drink.new(),Drink.new(),Drink.new()] as Array[Drink])
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- table.gd
+++ table.gd
@@ -2,22 +2,34 @@ extends Node2D
 
 var test_cup = preload('res://test_cup.tscn')
 var cups: Array[Node2D] = []
+var cup_location_goals = []
+var slide_start_time = 0
 
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	pass
+	if Time.get_ticks_msec() < slide_start_time + slide_time:
+		slide_cups()
 
 var temp_cup_height = 300 #temporary placement variable
-func slide_cups(drinks: Array[Drink]) -> void:
+var temp_cup_width = 90 #ditto
+var slide_time = 200
+
+func begin_slide_cups(drinks: Array[Drink]) -> void:
+	slide_start_time = Time.get_ticks_msec()
 	for i in drinks.size():
 		var drink = drinks[i]
 		var new_cup = test_cup.instantiate()
 		cups.append(new_cup)
 		#somehow link cup to the drink data structure here
 		add_child(new_cup)
-		new_cup.global_position = Vector2(get_cup_spacing(i,drinks.size()),global_position.y + get_parent().SCREEN_SIZE.y - temp_cup_height) #Will change this get_parent call later, screen size will prob be made global somehow
+		cup_location_goals.append(get_cup_spacing(i,drinks.size()))
 
 func get_cup_spacing(i: int, num_cups: int) -> float:
-	return global_position.x + (i / float(num_cups)) * get_parent().SCREEN_SIZE.x
+	return ((i+1) / float(num_cups+1)) * get_parent().SCREEN_SIZE.x - temp_cup_width
+
+func slide_cups() -> void:
+	print((Time.get_ticks_msec() - slide_start_time)/slide_time)
+	for i in cups.size():
+		cups[i].global_position = Vector2(global_position.x + 1 * cup_location_goals[i],global_position.y + get_parent().SCREEN_SIZE.y - temp_cup_height) #Will change this get_parent call later, screen size will prob be made global somehow
