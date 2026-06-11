8762baba6188e6dc20163846442a6a75e96cf2fa
MstrMinecrafter <coxsimon621@gmail.com>
2026-06-10 20:11:04 -0700

    Poison bar WIP

--- 03_VisualInputOutput/04_PoisonBar/poison_bar.gd
+++ 03_VisualInputOutput/04_PoisonBar/poison_bar.gd
@@ -2,9 +2,13 @@ class_name PoisonBar
 extends Node2D
 
 @onready var _input: Label = $Input
+@onready var _liquid: Polygon2D = $Liquid
 
 func _ready() -> void:
 	GS.poison_set.connect(set_value)
 
 func set_value(old_val: int, new_val: int) -> void:
 	_input.text = str(new_val)
+	var mat = _liquid.material as Material
+	mat.set_shader_parameter("level",new_val/12.0)
+	mat.set_shader_parameter("max_height",_liquid.polygon[3].y)
