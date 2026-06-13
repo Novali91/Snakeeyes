000bfd095103ed3d75515b9295e35d17808a4c0b
Avery Simmons <eno116116@gmail.com>
2026-06-13 00:04:27 -0700

    python fix

--- 01_Managers/01_StateManager/01_States/02_Play/ability_helper.gd
+++ 01_Managers/01_StateManager/01_States/02_Play/ability_helper.gd
@@ -149,8 +149,8 @@ func trigger_ability(ind: int, drink_position: Vector2, cur_drink: DrinkResource
 			sm.camera_manager.switch_screen(sm.camera_manager.MIDDLE, true)
 			sm.camera_manager.unlock_camera()
 		PYTHON:
-			GS.set_charm(GS.get_charm() + floor((cur_drink.strength)/2.0))
 			sm.charm_overlay.gain_charm(GS.get_charm() + floor((cur_drink.strength)/2.0), drink_position)
+			GS.set_charm(GS.get_charm() + floor((cur_drink.strength)/2.0))
 		SCARLET_SNAKE:
 			pass
 		HOGNOSE_SNAKE:
