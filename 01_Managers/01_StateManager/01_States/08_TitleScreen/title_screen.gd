142c8de66d12f513d2d151a3ef11a1684e05c4f9
MstrMinecrafter <coxsimon621@gmail.com>
2026-06-13 22:23:24 -0700

    Implemented game and menu loops (not trap)

--- 01_Managers/01_StateManager/01_States/08_TitleScreen/title_screen.gd
+++ 01_Managers/01_StateManager/01_States/08_TitleScreen/title_screen.gd
@@ -6,11 +6,12 @@ func setup() -> void:
 
 func enter() -> void:
 	sm.camera_manager.lock_camera()
+	GS.sound_manager.play_menu()
 
 func exit() -> void:
 	pass
 
 func _play_clicked() -> void:
 	sm.title_screen_manager.queue_free()
-	
+	GS.sound_manager.play_game()
 	sm.switch_state(sm.States.SETUP_TURN)
--- 01_Managers/11_SoundManager/sound_manager.gd
+++ 01_Managers/11_SoundManager/sound_manager.gd
@@ -9,11 +9,22 @@ extends Node2D
 @onready var dice: AudioStreamPlayer = $Dice
 @onready var fall: AudioStreamPlayer = $Fall
 @onready var slide: AudioStreamPlayer = $Slide
+@onready var game: AudioStreamPlayer = $Music/Game
+@onready var menu: AudioStreamPlayer = $Music/Menu
+@onready var trap: AudioStreamPlayer = $Music/Trap
+
+var _prev_music: AudioStreamPlayer
+var _cur_music: AudioStreamPlayer
 
 var jingles: Array[AudioStreamPlayer]
 
 var _bell_base_pitch: float
 @export var bell_pitch_step: float
+
+var _transition_progress: float = -1.0
+@export var transition_time: float
+@export var music_vol: float
+const MUTE_DB: float = -100.0 #just some very large negative number
 
 func _ready() -> void:
 	GS.sound_manager = self
@@ -21,6 +32,16 @@ func _ready() -> void:
 	for child in $Jingles.get_children():
 		jingles.append(child as AudioStreamPlayer)
 
+func _process(delta: float) -> void:
+	if _transition_progress >= 0.0 and _transition_progress < transition_time:
+		_transition_progress = clamp(_transition_progress + delta,0.0,transition_time)
+		var prog_proportion = _transition_progress / transition_time
+		if _cur_music != null:
+			_cur_music.volume_db = lerp(MUTE_DB,music_vol,prog_proportion)
+		if _prev_music != null:
+			_prev_music.volume_db = lerp(music_vol,MUTE_DB,prog_proportion)
+	if _prev_music != null and _prev_music.volume_db == MUTE_DB:
+		_prev_music.stop()
 func play_clink() -> void:
 	clink.play(0.2)
 	
@@ -48,3 +69,19 @@ func play_slide() -> void:
 
 func play_jingle(index: int) -> void:
 	jingles[index].play()
+
+func play_game() -> void:
+	play_music(game)
+
+func play_menu() -> void:
+	play_music(menu)
+
+func play_trap() -> void:
+	play_music(trap)
+
+func play_music(track: AudioStreamPlayer) -> void:
+	_transition_progress = 0.0
+	track.play()
+	track.volume_db = MUTE_DB
+	_prev_music = _cur_music
+	_cur_music = track
