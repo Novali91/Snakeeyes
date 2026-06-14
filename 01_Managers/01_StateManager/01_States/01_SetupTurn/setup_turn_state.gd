57f4d5da4b832180990e3ba6d83be4636a32f0e9
MstrMinecrafter <coxsimon621@gmail.com>
2026-06-14 01:43:08 -0700

    Themed attack markers

--- 01_Managers/01_StateManager/01_States/01_SetupTurn/setup_turn_state.gd
+++ 01_Managers/01_StateManager/01_States/01_SetupTurn/setup_turn_state.gd
@@ -16,7 +16,7 @@ func enter() -> void:
 	if sm.attack_manager._current_attack == null or sm.attack_manager.get_attack().ability_index != 10:
 		sm.attack_manager.next_attack()
 	sm.score_bar.set_goal_value(sm.attack_manager.get_attack_goal())
-	sm.player_strength.place_attack_tickers(sm.attack_manager.get_attack().damage)
+	sm.player_strength.place_attack_tickers(sm.attack_manager.get_attack().damage,sm.attack_manager._next_attack.damage)
 	GS.cur_attack_index = sm.attack_manager.get_attack().ability_index
 	
 	GS.turn_count += 1
--- 03_VisualInputOutput/05_PlayerStrength'/player_strength.gd
+++ 03_VisualInputOutput/05_PlayerStrength'/player_strength.gd
@@ -2,6 +2,7 @@ class_name PlayerStrength
 extends Node2D
 
 const _ATTACK_TICKER_SCENE = preload("res://03_VisualInputOutput/05_PlayerStrength'/attack_ticker.tscn")
+const _NEXT_ATTACK_TICKER_SCENE = preload("res://03_VisualInputOutput/05_PlayerStrength'/next_attack_ticker.tscn")
 const _TICKER_Y_OFFSET = -200
 const _TICKER_VALUES = [
 	0,
@@ -31,6 +32,7 @@ var _tween: Tween
 var _markers: Array[Marker2D]
 
 var _attack_tickers: Array[Node2D] = []
+var _next_attack_tickers: Array[Node2D] = []
 
 func _ready() -> void:
 	GS.strength_set.connect(set_value)
@@ -38,17 +40,31 @@ func _ready() -> void:
 	for m: Marker2D in _marker_node.get_children():
 		_markers.push_back(m)
 
-func place_attack_tickers(attacks: Array[int]) -> void:
+func place_attack_tickers(attacks: Array[int], next_attacks: Array[int]) -> void:
 	for attack_ticker in _attack_tickers:
 		attack_ticker.queue_free()
+	for next_attack_ticker in _next_attack_tickers:
+		next_attack_ticker.queue_free()
 	_attack_tickers = []
+	_next_attack_tickers = []
 	var attack_total = 0
 	for attack in attacks:
 		attack_total += attack
 		var temp_attack_ticker = _ATTACK_TICKER_SCENE.instantiate()
 		add_child(temp_attack_ticker)
+		temp_attack_ticker.get_child(1).set_value(attack_total)
 		temp_attack_ticker.position.y = get_height(attack_total) - _TICKER_Y_OFFSET
 		_attack_tickers.append(temp_attack_ticker)
+	attack_total = 0
+	for next_attack in next_attacks:
+		attack_total += next_attack
+		var temp_next_attack_ticker = _NEXT_ATTACK_TICKER_SCENE.instantiate()
+		add_child(temp_next_attack_ticker)
+		temp_next_attack_ticker.get_child(1).set_value(attack_total)
+		temp_next_attack_ticker.position.y = get_height(attack_total) - _TICKER_Y_OFFSET
+		temp_next_attack_ticker.get_child(1).tint_col = Color.from_rgba8(30,30,30,255)
+		temp_next_attack_ticker.get_child(1).toggle_tint(true)
+		_next_attack_tickers.append(temp_next_attack_ticker)
 
 func get_height(val: int) -> float:
 	var stop_ind: int
@@ -78,6 +94,9 @@ func set_value(_old_val: int, new_val: int) -> void:
 	
 	_tween = create_tween().set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_OUT)
 	_tween.tween_property(_ticker, "position:y", ticker_target, 1)
+	
+	for attack_ticker in _attack_tickers:
+		attack_ticker.get_child(1).toggle_tint(ticker_target <= attack_ticker.position.y + _TICKER_Y_OFFSET)
 	
 	if _old_val != new_val && new_val > 0:
 		GS.sound_manager.play_bell(new_val)
